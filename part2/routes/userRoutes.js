96a48427cd5e7bb1f7323adeff299fcc2b9a396f
PundaZos <jasond0508@icloud.com>
2025-06-20 13:58:38 +0000

    [automatic commit] 2025-06-20 13:58:37 UTC 61.68.200.210

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -62,7 +62,9 @@ router.post('/login', async (req,red) => {
   try{
     const query = 'SELECT * FROM Users WHERE username = ? AND password_has = ?';
     const[result]=await db.query(query, [username,password]);
-    return;
+    if(!result.length){
+      
+    }
   }
 
   const userRecord = result[0];
