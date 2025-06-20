b479b3ab8e74eb169fabcd263e9bddcaf3d1d754
PundaZos <jasond0508@icloud.com>
2025-06-20 15:15:33 +0000

    [automatic commit] 2025-06-20 15:15:33 UTC 61.68.200.210

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -17,10 +17,10 @@ router.post('/register', async (req, res) => {
   const { username, email, password, role } = req.body;
 
   try {
-    const [result] = await db.query(
+    const [result] = await db.query(`
       INSERT INTO Users (username, email, password_hash, role)
       VALUES (?, ?, ?, ?)
-    , [username, email, password, role]);
+    `, [username, email, password, role]);
 
     res.status(201).json({ message: 'User registered', user_id: result.insertId });
   } catch (error) {
