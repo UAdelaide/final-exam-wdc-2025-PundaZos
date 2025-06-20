a0724528ddf86b204fca604d3059ad7f5b194d75
PundaZos <jasond0508@icloud.com>
2025-06-20 14:59:04 +0000

    [automatic commit] 2025-06-20 14:59:03 UTC 61.68.200.210

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -17,10 +17,10 @@ router.post('/register', async (req, res) => {
   const { username, email, password, role } = req.body;
 
   try {
-    const [result] = await db.query(`
+    const [result] = await db.query(
       INSERT INTO Users (username, email, password_hash, role)
       VALUES (?, ?, ?, ?)
-    `, [username, email, password, role]);
+    , [username, email, password, role]);
 
     res.status(201).json({ message: 'User registered', user_id: result.insertId });
   } catch (error) {
@@ -35,8 +35,38 @@ router.get('/me', (req, res) => {
   res.json(req.session.user);
 });
 
-//Login part
+// POST login (changed)
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
 
+  try {
+    const [rows] = await db.query(
+      SELECT user_id, username, role FROM Users
+      WHERE username = ? AND password_hash = ?
+    , [username, password]);
 
+    if (rows.length === 0) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+    req.session.user = rows[0];
+
+    res.json({ message: 'Login successful', user: rows[0] });
+  } catch (error) {
+    res.status(500).json({ error: 'Login failed' });
+  }
+});
+
+router.get('/login', (req, res) => {
+  const message = req.session.message || null;
+  req.session.message = null;
+  res.render('login', { message });
+});
+
+router.post('/logout', async (req, res) => {
+  req.session.destroy(() => {
+    res.clearCookie('connect.sid');
+    res.json({ message: 'log out gud' });
+  });
+});
 
 module.exports = router;
