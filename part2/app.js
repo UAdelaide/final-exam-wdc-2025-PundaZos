cfd5fc6e1e9ac95410a7bc697798f90d281f42d2
PundaZos <jasond0508@icloud.com>
2025-06-21 06:37:51 +0000

    [automatic commit] 2025-06-21 06:37:51 UTC 61.68.200.210

--- part2/app.js
+++ part2/app.js
@@ -34,7 +34,7 @@ app.get('/api/dogs', async(req,res) => {
       } else {
         const [dogs] = await db.excute(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
-            FROM Dogs d 
+            FROM Dogs d
             JOIN Users u ON d.owner_id = u.user_id`);
         await connection.end();
         res.json(rows);
