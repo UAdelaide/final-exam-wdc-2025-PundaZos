374b63a95eca2b90eae526ed51c1b74a6722e22f
PundaZos <jasond0508@icloud.com>
2025-06-21 06:38:12 +0000

    [automatic commit] 2025-06-21 06:38:12 UTC 61.68.200.210

--- part2/app.js
+++ part2/app.js
@@ -30,7 +30,8 @@ app.get('/api/dogs', async(req,res) => {
       if (req.query.owner) {
         const [dogs]=await db.execute(`
             SELECT dog_id, name FROM Dogs d
-            JOIN Users u ON d.owner_id = u.user_id WHERE u.username = ?`, [req.query.owner])
+            JOIN Users u ON d.owner_id = u.user_id WHERE u.username = ?`, [req.query.owner]);
+            
       } else {
         const [dogs] = await db.excute(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
